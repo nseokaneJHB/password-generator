ad749f0dd26e3120cc5f152173560042f3242af3
nolan <nolanseokane@gmail.com>
2024-07-26 01:03:18 +0200

    password generator complete (can be improved)

---
+++ README.md
@@ -0,0 +1,34 @@
+# Password generator app
+
+![Design preview for this interview](./preview.jpg)
+
+## The challenge
+
+Your challenge is to build out this password generator app and get it looking as close to the design as possible.
+
+You can use any tools you like to help you complete the challenge. So if you've got something you'd like to practice, feel free to give it a go.
+
+Your users should be able to:
+
+- Generate a password based on the selected inclusion options
+- Copy the generated password to the computer's clipboard
+- See a strength rating for their generated password
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+
+## Where to find everything
+
+Your task is to build out the project to the design file provided. We provide both Sketch and Figma versions of the design, so you can choose which tool you prefer to use. Each design comes with a `README.md` file as well to help you get set up. The design system in the design file will give you more information about the various colors, fonts, and styles used in this project. 
+
+
+All the required assets for this project are in the `/assets` folder. The images are already exported for the correct screen size and optimized. 
+
+We also include variable and static font files for the required fonts for this project. You can choose to either link to Google Fonts or use the local font files to host the fonts yourself. Note that we've removed the static font files for the font weights that aren't needed for this project.
+
+The `/starter-code` is, as you might have figured a decent place to start!
+
+You may decide to use a front-end framework...or not. 
+
+## Submitting your project
+
+You can just zip it up and email it back! You don't need to actually deploy this anywhere - if there is a procces to run a development server, you can drop it into this readme.
---
+++ starter-code/assets/js/index.js
@@ -0,0 +1,151 @@
+// Initialize variables
+const passwordValue = document.getElementById("password-value");
+const copyButton = document.getElementById("copy");
+
+const lengthValue = document.getElementById("length-value");
+const lengthInput = document.getElementById("length-input");
+
+const uppercasesCheckbox = document.getElementById("uppercases");
+const lowercasesCheckbox = document.getElementById("lowercases");
+const numbersCheckbox = document.getElementById("numbers");
+const symbolsCheckbox = document.getElementById("symbols");
+
+const strengthValue = document.getElementById("strength-value");
+const strengthRectangles = document.getElementsByClassName(
+	"wrapper-password-strength-rectangle"
+);
+
+const generatePasswordButton = document.getElementById(
+	"generate-password-button"
+);
+
+const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "/[!@#$%^&*()_+{}[]:;<>,.?/~\\-]/";
+
+const generatePassword = () => {
+	let characters;
+
+	if (uppercasesCheckbox.checked) {
+		characters += uppercaseLetters;
+	}
+
+	if (lowercasesCheckbox.checked) {
+		characters += lowercaseLetters;
+	}
+
+	if (numbersCheckbox.checked) {
+		characters += numbers;
+	}
+
+	if (symbolsCheckbox.checked) {
+		characters += symbols;
+	}
+
+	let password;
+	for (let i = 0; i < lengthInput.value; i++) {
+		if (!characters || characters.length < 1) {
+			alert(
+				"Please specify what to include on your password by checking at least one checkbox."
+			);
+			return;
+		}
+
+		const generatedCharacter = characters.charAt(
+			Math.floor(Math.random() * characters.length)
+		);
+
+		if (!password) {
+			password = generatedCharacter;
+		} else {
+			password += generatedCharacter;
+		}
+	}
+
+	return password;
+};
+
+const passwordStrengthCheck = (password) => {
+	let strength = 0;
+
+	if (/[a-z]/.test(password) && /[A-Z]/.test(password)) {
+		strength++;
+	}
+
+	if (/[0-9]/.test(password)) {
+		strength++;
+	}
+
+	if (/[!@#$%^&*()_+{}\[\]:;<>,.?/~\\-]/.test(password)) {
+		strength++;
+	}
+
+	if (password.length >= 8) {
+		strength++;
+	}
+
+	if (strength <= 1) {
+		return "to weak";
+	} else if (strength === 2) {
+		return "weak";
+	} else if (strength === 3) {
+		return "medium";
+	} else {
+		return "strong";
+	}
+};
+
+const updatePasswordStrength = () => {
+	const password = generatePassword();
+	const passwordStrength = passwordStrengthCheck(password);
+
+	const strengthClasses = {
+		"to weak": ["to-weak"],
+		weak: ["weak", "weak"],
+		medium: ["medium", "medium", "medium"],
+		strong: ["strong", "strong", "strong", "strong"],
+	};
+
+	const strengthRectanglesArray = Array.from(strengthRectangles);
+
+	// Clear existing strength classes
+	strengthRectanglesArray.forEach((rect) => {
+		rect.classList.remove("to-weak", "weak", "medium", "strong");
+	});
+
+	// Add new strength classes based on the password strength
+	if (strengthClasses[passwordStrength]) {
+		strengthClasses[passwordStrength].forEach((cls, index) => {
+			if (strengthRectanglesArray[index]) {
+				strengthRectanglesArray[index].classList.add(cls);
+			}
+		});
+	}
+
+	return passwordStrength;
+};
+
+// Initialize values on load
+lengthValue.innerHTML = lengthInput.value;
+passwordValue.innerHTML = generatePassword();
+strengthValue.innerHTML = updatePasswordStrength();
+
+// Add event listeners
+lengthInput.addEventListener("input", (event) => {
+	const length = event.target.value;
+	lengthValue.innerHTML = length;
+});
+
+generatePasswordButton.addEventListener("click", () => {
+	const password = generatePassword();
+	passwordValue.innerHTML = password;
+	strengthValue.innerHTML = updatePasswordStrength();
+});
+
+copyButton.addEventListener("click", () => {
+	navigator.clipboard.writeText(passwordValue.innerHTML);
+
+	alert("Password copied to clipboard.");
+	return;
+});
